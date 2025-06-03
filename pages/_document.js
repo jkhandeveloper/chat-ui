7ea2615bf332af0226d6d180369c49f864d73421
Jehangir Khan <jkhandeveloper@gmail.com>
2025-06-03 17:33:24 +0500

    Initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        {/* Inter Font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
+        {/* Bootstrap CDN */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body style={{ fontFamily: "Inter, sans-serif" }}>
         <Main />
         <NextScript />
       </body>
